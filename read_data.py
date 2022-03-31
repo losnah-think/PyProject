49069823be5574fc523bea2da07822fb366b485c
최한솔 <hansol416@naver.com>
2022-03-31 15:58:08 +0900

    data_frame 분할 및 시각화 연습

--- read_data.py
+++ read_data.py
@@ -1,5 +1,31 @@
 import pandas as ps
+import matplotlib.pyplot as plt
 
-data_fm = ps.read_csv("./data/국민건강보험공단_한국인 혈압 참조표준_20201205.csv", sep=",", encoding="cp949")
+df = ps.read_csv("./data/국민건강보험공단_한국인 혈압 참조표준_20201205.csv", sep=",", encoding="cp949")
 
-print(data_fm.head())
+# 데이터 출력 연습
+# print(df.head())
+
+# 데이터 자르기 연습
+# print(df.iloc[0:-1,2:16].head())
+
+# 데이터 요약
+# print(df['측정수(건)'].describe())
+
+# 최소값으로부터 구간별 분류
+section = list(range(0,362524,1000))
+data_label = [str(x)+"이상 "+str(x+1000)+"미만" for x in section]
+
+# 레이블링 확인용
+# print(data_label)
+
+# cut 을 통해 구간을 나눌 수 있음
+df["측정값 범위"] = ps.cut(df["측정수(건)"], section, right=False, labels=data_label[:-1])
+print(df.head())
+print(df["측정값 범위"].describe())
+
+
+# 그래프 그리기
+plt.plot([2,3,5,10])
+plt.plot(df['측정수(건)'])
+plt.show()
