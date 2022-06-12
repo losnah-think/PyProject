bc216b8923dfa41a46112ab70142ee9e74398721
최한솔 <hansol416@tu.ac.kr>
2022-06-12 17:09:57 +0900

    왜 Count 가 증가 안하는지 이유 찾기

--- myClass2.py
+++ myClass2.py
@@ -1,12 +1,22 @@
+from itertools import count
+
+
 class Animal:
+    count = 0
+
     def __init__(self, name):
         self.name = name
+        self.count += 1 
     
     def move(self):
         print("총총총")
 
     def speak(self):
         pass
+
+    @classmethod
+    def showCount(Animal):
+        print(Animal.count)
 
 class Dog (Animal):
     def speak(self):
@@ -26,3 +36,5 @@ animals = [Dog('멍멍'),Cat('냥냥')]
 
 for a in animals:
     a.speak()
+
+Animal.showCount()
